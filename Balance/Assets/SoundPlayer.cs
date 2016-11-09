50b410654199ce86d6d6dcd4e72fa92ca85a3e1f
Dong <dongmai111@yahoo.com>
2016-11-09 00:08:54 -0500

    Updated

---
+++ Balance/Assets/SoundPlayer.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundPlayer : MonoBehaviour {
+
+	AudioSource[] audioSources;
+	AudioSource background;
+	AudioSource menuSelect;
+	AudioSource movingKeyPressed;
+	AudioSource movingSound;
+	// Use this for initialization
+	void Start () {
+		Object.DontDestroyOnLoad(this);
+		audioSources = GetComponents<AudioSource> ();
+		background = audioSources [0];
+		menuSelect = audioSources [1];
+		movingKeyPressed = audioSources [2];
+		movingSound = audioSources [3];
+		playbackgroundMusic ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void playbackgroundMusic() {
+		background.Play ();
+	}
+
+	public void playMenuSelect() {
+		menuSelect.Play ();
+	}
+
+	public void playMovingKeyPressed() {
+		movingKeyPressed.Play ();
+	}
+
+	public void playMovingSound() {
+		movingSound.Play ();
+	}
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# Balance
+Group Project
+
+# Contributors 
+  <ul>
+    <li>Tarek Saidee</li>
+    <li>Solon Miranda</li>
+    <li>Dong Mai</li>
+  </ul>
